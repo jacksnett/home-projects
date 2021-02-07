d54bd63a30cac26a7f9fdac43a85b890d7d0854c
jacksnett <Yaakovsnett2@gmail.com>
2021-02-07 09:19:02 +0200

    added

---
+++ vue/vuejs-cli/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>let us guess how u feel</h1>
+    <div>u feel {{ feelingArray[feeling] }}</div>
+    <button @click="increment()">feeling better</button>
+  </div>
+</template> 
+
+<script>
+export default {
+  data(){
+    return {
+      feeling: 0,
+      feelingArray: [
+        'sad',
+        'ok',
+        'bourd',
+        'fine',
+        'good',
+        'great',
+        'amazing'
+      ]
+    }
+  },
+  methods: {
+    increment() {
+      this.feeling = (this.feeling + 1) % this.feelingArray.length;
+    }
+  }
+}
+</script>
+
+<style>
+</style>
